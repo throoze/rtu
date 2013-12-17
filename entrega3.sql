3c3d871e9ca2301490e102d942c82de45bd01820
Victor De Ponte <rdbvictor19@gmail.com>
2013-12-17 16:59:35 -0430

    Mod: Rectificado el documento, con todos los tipos definidos. Add: Enunciado del proyecto.

--- entrega3.sql
+++ entrega3.sql
@@ -1,73 +1,128 @@
-CREATE OR REPLACE TYPE fecha OBJECT(
-	dia VARCHAR(2),
-	mes VARCHAR(3),
-  	año VARCHAR(4)
+/* Entrega 3
+ * Autores:	Alberto Cols
+ * 					Victor De Ponte
+ * 					Ivan Travecedo	08-11131
+ *
+ *
+ * NOTA: REVISAR NULL/NOT NULL para atributos y colocarlos
+ * 			 La temperatura estara expresada en en un numero sin unidades?
+ *       No he colocado ninguno de los atributos derivados
+ *       Add constrains para primary key
+ */
+
+-- Tipo de datos
+
+DROP TYPE tabla_tipoHito FORCE;
+DROP TYPE tabla_idiomas FORCE;
+DROP TYPE tabla_telefonos FORCE;
+DROP TYPE turista FORCE;
+DROP TYPE guia FORCE;
+DROP TYPE destino FORCE;
+DROP TYPE costo FORCE;
+DROP TYPE tabla_costo FORCE;
+DROP TYPE informacion FORCE;
+DROP TYPE tabla_informacion FORCE;
+DROP TYPE lista_dias FORCE;
+DROP TYPE servicio FORCE;
+DROP TYPE hito FORCE;
+DROP TYPE ruta FORCE;
+
+
+-- Tipo que es una tabla de referencias a tipos de hito
+CREATE OR REPLACE TYPE tabla_tipoHito_t AS TABLE of VARCHAR2(20);
+/
+
+-- Tipo que es una tabla de referencias a idiomas
+CREATE OR REPLACE TYPE tabla_idiomas_t AS TABLE of VARCHAR2(20);
+/
+
+-- Tipo que es una tabla de referencias a telefonos
+CREATE OR REPLACE TYPE tabla_telefonos_t AS TABLE of NUMBER;
+/
+
+-- Tipo turista
+CREATE OR REPLACE TYPE turista_t AS OBJECT (
+	activo							VARCHAR2(2),
+	apellido						VARCHAR2(30),
+	contrasena					VARCHAR2(20),
+	fechaRegistro				DATE,
+	genero							VARCHAR2(6),
+	mail								VARCHAR2(30),
+	tipoHitosPreferidos	tabla_tipoHito_t,
+  nombre              VARCHAR2(30),
+  username            VARCHAR2(20)
+) NOT FINAL;
+/
+
+
+-- Tipo guia
+CREATE OR REPLACE TYPE guia_t UNDER turista_t (
+	idiomas 	tabla_idiomas_t,
+	telefonos tabla_telefonos_t
 );
 /
-CREATE OR REPLACE TYPE direccion OBJECT(
-	calle VARCHAR(50),
-	ciudad VARCHAR(50),
-  	estado VARCHAR(50)
-	codPost VARCHAR(50),
-	pais VARCHAR(50)
+
+-- Tipo destino
+CREATE OR REPLACE TYPE destino_t AS OBJECT (
+	descripcion	VARCHAR2(100),
+	nombre			VARCHAR2(30)
+) NOT FINAL;
+/
+
+-- Tipo para tipos de hito
+CREATE OR REPLACE TYPE costo_t AS OBJECT (
+	publico	VARCHAR2(20),
+	monto		NUMBER
 );
 /
-CREATE OR REPLACE TYPE visitante  OBJECT(
-	nombre1 VARCHAR(50),
-	nombre2 VARCHAR(50),
-	ci VARCHAR(10),
-	email VARCHAR(50),
-	fecha_nac fecha,
-	sexo VARCHAR(1),
-	ocupacion VARCHAR(50),
-	tlf movil VARCHAR(11),
-	flf fijo VARCHAR(11),
-	direccion direccion,
-	nacionalidad VARCHAR(50),
-	edo_civil VARCHAR(50)
-		
+-- Tipo que es una tabla de referencias a costos
+CREATE OR REPLACE TYPE tabla_costo_t AS TABLE of REF costo_t;
+/
+
+-- Tipo para tipos de hito
+CREATE OR REPLACE TYPE informacion_t AS OBJECT (
+	tipo 			VARCHAR2(20),
+	contacto	VARCHAR2(100)
 );
 /
-CREATE OR REPLACE TYPE ruta OBJECT(
-	nombre VARCHAR(50),
-	ciudad VARCHAR(50),
-	disponible VARCHAR(10),
-	tiempo_recorrido VARCHAR(50),
-	total_visitas VARCHAR(1),
-	horario_recom VARCHAR(50),
-	fecha_reg_bd fecha,
-	rating VARCHAR(11)
-		
+
+-- Tipo que es una tabla de referencias a costos
+CREATE OR REPLACE TYPE tabla_informacion_t AS TABLE of REF informacion_t;
+/
+
+CREATE OR REPLACE TYPE lista_dias_t AS VARRAY(7) of VARCHAR2(10);
+/
+
+-- Tipo para servicio
+CREATE OR REPLACE TYPE servicio_t UNDER destino_t (
+	costo								tabla_costo_t,
+	estado							VARCHAR2(13),
+	informacionContacto	tabla_informacion_t,
+	tipo 								VARCHAR2(20),
+	dia									lista_dias_t,
+	duracion						DATE,
+	fechaInicio					DATE,
+	fechaFin						DATE,
+	horaComienzo				DATE
 );
 /
-CREATE OR REPLACE TYPE hito OBJECT(
-	nombre VARCHAR(50),
-	categoria VARCHAR(50),
-	horario VARCHAR(10),
-	estado VARCHAR(50),
-	total_visitas VARCHAR(1),
-	fecha_apertura fecha,
-	fecha_cierre fecha
+
+-- Tipo para hito
+CREATE OR REPLACE TYPE hito_t AS OBJECT (
+	categoria		tabla_tipoHito_t,
+	estado			VARCHAR2(30),
+	pago				tabla_costo_t,
+	publico			VARCHAR2(30),
+	temperatura	NUMBER,
+	vestimenta	VARCHAR2(30)
 );
 /
-CREATE OR REPLACE TYPE via OBJECT(
-	nombre VARCHAR(50),
-	tipo VARCHAR(50),
-	abierto VARCHAR(10),
-	peatones VARCHAR(50),
-	automoviles VARCHAR(50)
-		
-);
-/
-CREATE OR REPLACE TYPE ubicacion OBJECT(
-	coordenada VARCHAR(50),
-	altura VARCHAR(50)
-		
-);
-/
-CREATE OR REPLACE TYPE ubicacion OBJECT(
-	tipo VARCHAR(50),
-	reportado VARCHAR(50)
-		
+
+-- Tipo para ruta
+CREATE OR REPLACE TYPE ruta_t AS OBJECT (
+	fechaRegistro  DATE,
+	nombre         VARCHAR2(30),
+  tipo           tabla_tipoHito_t,
+  creador        REF turista_t
 );
 /
