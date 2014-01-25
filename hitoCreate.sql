de9b6609de0794bce6193f5e5663657cbdaa727d
ivantrave <ivantrave@gmail.com>
2014-01-25 14:37:39 -0430

    se agrego a tablas la llave primaria a la tabla de Costos. En tipos se agrego un nuevo tipo de tablas de costos. hitoCreate tiene el procedicimento para agregar nuevos hitos a la BD

--- hitoCreate.sql
+++ hitoCreate.sql
@@ -2,16 +2,31 @@ INSERT INTO Costo VALUES ('Ninos',200);
 INSERT INTO Costo VALUES ('Adultos',300);
 INSERT INTO Costo VALUES ('Viejos',400);
 
-INSERT INTO Hito VALUES
-('La estatua del libertador','Estatua Simon Bolivar',tabla_tipoHito_t('Academico','Cultural','Historico'),'Abierto',
-  (SELECT tabla_costo_t(REF(c)) FROM Costo c WHERE publico='Ninos' AND monto=200),
-  'Todo publico',20,'Deportiva');
+DROP TABLE listaDeCostos;
 
---descripcion
---nombre
---categoria
---estado
---pago
---publico
---temperatura
---vestimenta
+CREATE GLOBAL TEMPORARY TABLE listaDeCostos (
+  publico VARCHAR2(20),
+  monto   NUMBER
+);
+
+CREATE OR REPLACE PROCEDURE nuevoHito(descripcion IN VARCHAR2,
+                                      nombre IN VARCHAR2,
+                                      categorias IN tabla_tipoHito_t,
+                                      estado IN VARCHAR2,
+                                      cost IN lista_costos_t,
+                                      publico IN VARCHAR2,
+                                      temperatura IN NUMBER,
+                                      vestimenta IN VARCHAR2) AS
+tabla_de_costos tabla_costo_t;
+BEGIN
+  FOR i IN cost.FIRST..cost.LAST
+  LOOP
+    INSERT INTO listaDeCostos VALUES (cost(i).publico,cost(i).monto);
+  END LOOP;
+
+  SELECT REF(c) bulk collect into tabla_de_costos FROM Costo c, listaDeCostos l
+     WHERE c.publico = l.publico AND c.monto = l.monto;
+
+INSERT INTO Hito VALUES (descripcion,nombre,categorias,estado,tabla_de_costos,publico,temperatura,vestimenta);
+END nuevoHito;
+/
