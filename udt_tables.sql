bcc53fb44bc37a649e28b25be89f543d1a3f86f3
Victor De Ponte <rdbvictor19@gmail.com>
2014-01-29 13:51:47 -0430

    mistico

--- udt_tables.sql
+++ udt_tables.sql
@@ -247,8 +247,7 @@ CREATE TABLE Ruta OF ruta_t (
   fechaRegistro       NOT NULL,
   nombre              NOT NULL,
   CONSTRAINT PK_RUTA PRIMARY KEY (nombre),
-  CONSTRAINT C_RUTA_FECHA_REGISTRO CHECK (nombre LIKE '^[a-zA-Z]{1,20}')     --String valido
-
+  CONSTRAINT C_RUTA_FECHA_REGISTRO CHECK (nombre LIKE '^[a-zA-Z]{1,20}')
 ) OBJECT ID PRIMARY KEY
   NESTED TABLE tipo STORE AS ruta_tipoHito
   NESTED TABLE hitos STORE AS ruta_to_hitos;
