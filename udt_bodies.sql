52f39958d7fc90adc6cce3d3962b9997a4b13f03
Victor De Ponte <rdbvictor19@gmail.com>
2014-01-29 04:23:50 -0430

    Fix:ALTER TYPE hito_t ADD MEMBER FUNCTION obtenerRutas RETURN tabla_compone_t CASCADE * ERROR at line 1: ORA-22324: altered type has compilation errors ORA-22328: object "CI531101"."HITO_T" has errors. PLS-00201: identifier 'TABLA_COMPONE_T' must be declared

--- udt_bodies.sql
+++ udt_bodies.sql
@@ -2,7 +2,7 @@
 ALTER TYPE ruta_t ADD MEMBER FUNCTION obtenerHitos RETURN tabla_hito_t CASCADE;
 
 -- Método que devuelve todas las Rutas de las cuales forma parte este Hito.
-ALTER TYPE hito_t ADD MEMBER FUNCTION obtenerRutas RETURN tabla_compone_t CASCADE;
+ALTER TYPE hito_t ADD MEMBER FUNCTION obtenerRutas RETURN tabla_ruta_t CASCADE;
 
 -- Método que devuelve todos los Servicios ofrecidos en esta ruta esta ruta.
 ALTER TYPE ruta_t ADD MEMBER FUNCTION obtenerServicios RETURN tabla_servicio_t CASCADE;
