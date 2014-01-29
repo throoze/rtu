548b40305ca04fb0b74d19d103d90992189299b8
ivantrave <ivantrave@gmail.com>
2014-01-29 13:32:31 -0430

    cambios en udt_test

--- udt_test.sql
+++ udt_test.sql
@@ -3,7 +3,6 @@ DECLARE
 	ct tabla_costo_t;
 	it tabla_informacion_t;
 	rt ruta_t;
-	tu_c turista_t;
 	tu_r REF turista_t;
 	tabla tabla_servicio_t;
 
@@ -15,30 +14,12 @@ INSERT INTO Costo VALUES ('General',0);
 INSERT INTO Informacion VALUES('Telefono','04123333333');
 INSERT INTO Informacion VALUES('FAX','04123333335');
 INSERT INTO Informacion VALUES('Mail','hola@chao.com');
-hito_t.crearHito('La estatua del libertador',
-					'Estatua Simon Bolivar',
-					tabla_tipoHito_t('Academico','Cultural'),
-					'Abierto',
-					lista_costos_t(costo_t('Ninos',200),
-					costo_t('Adultos',300)),
-					'Todo publico',
-					20,'Formal');
-
-hito_t.crearHito('Biblioteca de la USB',
-					'Biblioteca central',
-					tabla_tipoHito_t('Academico'),
-					'Abierto',
-					lista_costos_t(costo_t('General',0)),
-					'Todo publico',
-					16,'Casual');
-
-INSERT INTO Turista VALUES('Si','Perez','contrasena',SYSDATE, 'Hombre', 'correo@correo.com', tabla_tipoHito_t('Academico'), 'Pedro',
-							'pedrito');
-SELECT VALUE(t) into tu_c FROM Turista t WHERE username='pedrito';
-SELECT REF(tu_c) INTO tu_r FROM DUAL;
+hito_t.crearHito('La estatua del libertador','Estatua Simon Bolivar',tabla_tipoHito_t('Academico','Cultural'),'Abierto',lista_costos_t(costo_t('Ninos',200),costo_t('Adultos',300)),'Todo publico',20,'Formal');
+hito_t.crearHito('Biblioteca de la USB','Biblioteca central',tabla_tipoHito_t('Academico'),'Abierto',lista_costos_t(costo_t('General',0)),'Todo publico',16,'Casual');
+INSERT INTO Turista VALUES('Si','Perez','contrasena',SYSDATE, 'Hombre', 'correo@correo.com', tabla_tipoHito_t('Academico'), 'Pedro','pedrito');
+SELECT REF(t) into tu_r FROM Turista t WHERE username='pedrito';
 SELECT REF(h) bulk collect into th FROM Hito h;
-INSERT INTO Ruta VALUES (SYSDATE, 'Conoce la Simon', tabla_tipoHito_t('Academico','Cultural'),tu_r,th);
-
+INSERT INTO Ruta VALUES (SYSDATE, 'Conoce la Simon', tabla_tipoHito_t('Academico','Cultural'),(SELECT REF(t) FROM Turista t WHERE username='pedrito'),tabla_hito_t((SELECT REF(h) FROM Hito h)));
 SELECT REF(c) bulk collect into ct FROM Costo c;
 SELECT REF(i) bulk collect into it FROM Informacion i;
 SELECT VALUE(r) into rt FROM Ruta r  WHERE nombre='Conoce la Simon';
